e39745a8d6a8987ea24f17993f41eba4a95f3873
David Ford <davidford@Brandons-Macbook-Pro-2.local>
2018-10-25 21:13:10 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,29 @@
 console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+
+cardsInPlay.push(cardOne);
+console.log("User flipped queen");
+
+var cardTwo = cards[1];
+
+cardsInPlay.push(cardTwo);
+console.log("User flipped king");
+
+var match;
+var noMatch;
+
+if( cardsInPlay.length === 2) { 
+	if( cardsInPlay[0] === cardsInPlay[1]) {
+		match = "You found a match!";
+		alert(match);
+	}
+	else {
+		noMatch = "Sorry try again.";
+		alert(noMatch);
+	}
+}
