fe3f1b70e320509cbd3dd04aa2f9550f83ab950b
David Ford <davidford@Brandons-Macbook-Pro-2.local>
2018-10-26 12:05:13 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -4,26 +4,42 @@ console.log("Up and running!");
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-
-cardsInPlay.push(cardOne);
-console.log("User flipped queen");
-
-var cardTwo = cards[1];
-
-cardsInPlay.push(cardTwo);
-console.log("User flipped king");
-
-var match;
-var noMatch;
-
-if( cardsInPlay.length === 2) { 
+var checkForMatch = function(){
 	if( cardsInPlay[0] === cardsInPlay[1]) {
-		match = "You found a match!";
-		alert(match);
+		console.log("You found a match!");
+		alert("You found a match!");
 	}
 	else {
-		noMatch = "Sorry try again.";
-		alert(noMatch);
+		console.log("Sorry try again.");
+		alert("Sorry try again.")
 	}
 }
+
+
+var flipCard = function(cardId) {
+
+/*var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("User flipped " + cardOne);
+
+var cardTwo = cards[1];
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardTwo);*/
+console.log("User flipped " + cards[cardId]);
+
+cardsInPlay.push(cards[cardId]);
+checkForMatch();
+}
+
+
+//var match;
+//var noMatch;
+
+//if( cardsInPlay.length === 2) { 
+
+
+flipCard(1);
+flipCard(2);
+
+
+
